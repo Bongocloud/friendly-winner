2e7f173c094bdc4a8751d89971ea3333e356574e
Nicole <52993145+Bongocloud@users.noreply.github.com>
2026-01-20 18:07:39 -0600

    Add files via upload

---
+++ hero.py
@@ -0,0 +1,31 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+Please ensure your code meets the following before submitting:
+
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Define a String variable (e.g., Character Name).
+[ ] 3. Define an Integer variable (e.g., Level or Age).
+[ ] 4. Define a Float variable (e.g., Speed or Power).
+[ ] 5. Define a Boolean variable (e.g., is_alive).
+[ ] 6. Print all variables to the screen using F-Strings.
+[ ] 7. Upload to GitHub and add instructor as a contributor.
+-----------------------------------------------------------------------
+"""
+# Nicole, 1/20/2026
+# Character information
+
+character_name = input("Type in your characters name: ")
+level = 0
+speed = 76.88
+is_alive = True
+
+# Print all variables in storyline
+
+print(f" Hi {character_name},")
+
+print(f"\nYou are on level {level} and it seems your life is {is_alive}")
+
+print (f"You seem to be quite fast as your speed is {speed}, may that help you on your journey.")
+
